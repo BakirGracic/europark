30a03447a12f98fd96b989197c72fe35b52fdbce
BakirGracic <bakirgdev@gmail.com>
2025-03-01 14:59:11 +0100

    prettier format

--- src/features/hero/components/Hero.tsx
+++ src/features/hero/components/Hero.tsx
@@ -8,8 +8,8 @@ export default function Hero({ children }: { children: React.ReactNode }) {
 		<div className='hero relative min-h-screen'>
 			<div className='hero-content z-50 text-center text-[#f7f5f3]'>
 				<div className='max-w-xl'>
-					<h1 className='text-5xl font-bold break-word'>{t('title')}</h1>
-					<p className='py-6 font-medium break-word'>{t('description')}</p>
+					<h1 className='break-word text-5xl font-bold'>{t('title')}</h1>
+					<p className='break-word py-6 font-medium'>{t('description')}</p>
 					<Link href='#reserve' className='btn btn-primary shadow-md'>
 						{t('cta')}
 					</Link>
