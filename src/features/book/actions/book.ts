c5b7d0b43e90adeca1920469bf5e92deb2ab9c90
BakirGracic <bakirgdev@gmail.com>
2025-03-01 16:50:39 +0100

    chore: add templates and finalize form

--- src/features/book/actions/book.ts
+++ src/features/book/actions/book.ts
@@ -2,6 +2,9 @@
 
 import { BookFormStatus } from '@/features/book/types/status';
 const nodemailer = require('nodemailer');
+const fs = require('fs');
+import path from 'path';
+import { readFileSync } from 'fs';
 
 export async function bookAction(
 	state: BookFormStatus,
@@ -13,7 +16,7 @@ export async function bookAction(
 		phone: formData.get('phone') as string,
 		date_from: formData.get('date_from') as string,
 		date_to: formData.get('date_to') as string,
-		message: formData.get('message') as string
+		message: (formData.get('message') as string) || '<nema poruke>'
 	};
 
 	const transporter = nodemailer.createTransport({
@@ -26,25 +29,76 @@ export async function bookAction(
 		}
 	});
 
-	// confirmation to user
 	transporter
 		.sendMail({
 			from: `"EuroPark d.o.o." <${process.env.NODEMAILER_SYSTEM_EMAIL}>`,
 			to: formFields.email,
 			subject: 'Vaša rezervacija je uspješno spašena!',
-			html: '<b>Hello world? TEST!</b>' // TODO napravi html temaplte za confirmation
+			text: readFileSync(
+				path.join(process.cwd(), 'src/features/book/templates/confirmation.txt'),
+				'utf8'
+			),
+			html: readFileSync(
+				path.join(process.cwd(), 'src/features/book/templates/confirmation.html'),
+				'utf8'
+			)
 		})
 		.catch(() => {
 			return { status: 'error' };
 		});
 
-	// reservation to system
+	const reservation_text = Object.entries({
+		'{{NAME}}': formFields.name,
+		'{{EMAIL}}': formFields.email,
+		'{{PHONE}}': formFields.phone,
+		'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		}),
+		'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		}),
+		'{{MESSAGE}}': formFields.message
+	}).reduce(
+		(txt, [placeholder, value]) => txt.replace(new RegExp(placeholder, 'g'), value),
+		readFileSync(
+			path.join(process.cwd(), 'src/features/book/templates/reservation.txt'),
+			'utf8'
+		)
+	);
+	const reservation_html = Object.entries({
+		'{{NAME}}': formFields.name,
+		'{{EMAIL}}': formFields.email,
+		'{{PHONE}}': formFields.phone,
+		'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		}),
+		'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		}),
+		'{{MESSAGE}}': formFields.message
+	}).reduce(
+		(html, [placeholder, value]) => html.replace(new RegExp(placeholder, 'g'), value),
+		readFileSync(
+			path.join(process.cwd(), 'src/features/book/templates/reservation.html'),
+			'utf8'
+		)
+	);
+
 	transporter
 		.sendMail({
 			from: `"EuroPark d.o.o." <${process.env.NODEMAILER_SYSTEM_EMAIL}>`,
 			to: process.env.NODEMAILER_RESERVATIONS_EMAIL,
 			subject: 'Nova Rezervacija [europark.ba]',
-			html: '<b>Hello world? TEST!</b>' // TODO napravi html temaplte za rezervaciju
+			text: reservation_text,
+			html: reservation_html
 		})
 		.catch(() => {
 			return { status: 'error' };
