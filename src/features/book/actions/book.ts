b2de7debda9d1bc412b465bf5bc9fa573ea00737
BakirGracic <bakirgdev@gmail.com>
2025-04-07 11:42:27 +0200

    fixes 2

--- src/features/book/actions/book.ts
+++ src/features/book/actions/book.ts
@@ -15,18 +15,18 @@ export async function bookAction(state: BookFormStatus, formData: FormData): Pro
 		message: (formData.get('message') as string) || '<nema poruke>'
 	};
 
-	const transporter = nodemailer.createTransport({
-		host: process.env.NODEMAILER_SERVER,
-		port: 465,
-		secure: true,
-		auth: {
-			user: process.env.NODEMAILER_SYSTEM_EMAIL,
-			pass: process.env.NODEMAILER_PASSWORD
-		}
-	});
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.NODEMAILER_SERVER,
+			port: 465,
+			secure: true,
+			auth: {
+				user: process.env.NODEMAILER_SYSTEM_EMAIL,
+				pass: process.env.NODEMAILER_PASSWORD
+			}
+		});
 
-	transporter
-		.sendMail({
+		transporter.sendMail({
 			from: `"EuroPark d.o.o." <${process.env.NODEMAILER_SYSTEM_EMAIL}>`,
 			to: formFields.email,
 			subject: 'Vaša rezervacija je uspješno spašena!',
@@ -38,61 +38,57 @@ export async function bookAction(state: BookFormStatus, formData: FormData): Pro
 				path.join(process.cwd(), 'src/features/book/templates/confirmation.html'),
 				'utf8'
 			)
-		})
-		.catch(() => {
-			return { status: 'error' };
 		});
 
-	const reservation_text = Object.entries({
-		'{{NAME}}': formFields.name,
-		'{{EMAIL}}': formFields.email,
-		'{{PHONE}}': formFields.phone,
-		'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		}),
-		'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		}),
-		'{{MESSAGE}}': formFields.message
-	}).reduce(
-		(txt, [placeholder, value]) => txt.replace(new RegExp(placeholder, 'g'), value),
-		readFileSync(path.join(process.cwd(), 'src/features/book/templates/reservation.txt'), 'utf8')
-	);
-	const reservation_html = Object.entries({
-		'{{NAME}}': formFields.name,
-		'{{EMAIL}}': formFields.email,
-		'{{PHONE}}': formFields.phone,
-		'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		}),
-		'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		}),
-		'{{MESSAGE}}': formFields.message
-	}).reduce(
-		(html, [placeholder, value]) => html.replace(new RegExp(placeholder, 'g'), value),
-		readFileSync(path.join(process.cwd(), 'src/features/book/templates/reservation.html'), 'utf8')
-	);
+		const reservation_text = Object.entries({
+			'{{NAME}}': formFields.name,
+			'{{EMAIL}}': formFields.email,
+			'{{PHONE}}': formFields.phone,
+			'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+			}),
+			'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+			}),
+			'{{MESSAGE}}': formFields.message
+		}).reduce(
+			(txt, [placeholder, value]) => txt.replace(new RegExp(placeholder, 'g'), value),
+			readFileSync(path.join(process.cwd(), 'src/features/book/templates/reservation.txt'), 'utf8')
+		);
+		const reservation_html = Object.entries({
+			'{{NAME}}': formFields.name,
+			'{{EMAIL}}': formFields.email,
+			'{{PHONE}}': formFields.phone,
+			'{{DATE_FROM}}': new Date(formFields.date_from).toLocaleDateString('bs-BA', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+			}),
+			'{{DATE_TO}}': new Date(formFields.date_to).toLocaleDateString('bs-BA', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+			}),
+			'{{MESSAGE}}': formFields.message
+		}).reduce(
+			(html, [placeholder, value]) => html.replace(new RegExp(placeholder, 'g'), value),
+			readFileSync(path.join(process.cwd(), 'src/features/book/templates/reservation.html'), 'utf8')
+		);
 
-	transporter
-		.sendMail({
+		transporter.sendMail({
 			from: `"EuroPark d.o.o." <${process.env.NODEMAILER_SYSTEM_EMAIL}>`,
 			to: process.env.NODEMAILER_RESERVATIONS_EMAIL,
 			subject: 'Nova Rezervacija [europark.ba]',
 			text: reservation_text,
 			html: reservation_html
-		})
-		.catch(() => {
-			return { status: 'error' };
 		});
+	} catch {
+		return { status: 'error' };
+	}
 
 	return { status: 'ok' };
 }
