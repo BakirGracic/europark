3329857a3632c6ad934b62eef60fd18ea402f2df
Bakir Gracić <bakirgdev@gmail.com>
2025-05-17 12:28:04 +0200

    update map embed link to new google business property

--- src/features/location/components/Map.tsx
+++ src/features/location/components/Map.tsx
@@ -2,7 +2,7 @@ export default function Map() {
 	return (
 		<div className='mx-auto aspect-video max-w-[700px]'>
 			<iframe
-				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d488.1585354145393!2d18.33137126571597!3d43.83081857668392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4758cb57a09be203%3A0x59380bdcd53ed568!2sEuropark!5e0!3m2!1sen!2sba!4v1740315367369!5m2!1sen!2sba'
+				src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1439.102803801294!2d18.330633355561886!3d43.83083458404374!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4758cb39c712393d%3A0xbcafbf50a30dd75c!2sParking%20Europark%20Sarajevo!5e0!3m2!1sen!2sba!4v1747477594261!5m2!1sen!2sba' 
 				className='rounded-box mx-auto h-full w-full border-none'
 				allowFullScreen={false}
 				loading='lazy'
