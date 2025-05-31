e901a0118a426d780418e8a208151a8c59889d3f
Bakir Gracić <bakirgdev@gmail.com>
2025-05-31 15:27:09 +0200

    update video iframe sizing

--- src/features/video/components/Video.tsx
+++ src/features/video/components/Video.tsx
@@ -11,14 +11,12 @@ export default function Video() {
 				<TruckIcon className='size-8' />
 			</h2>
 			<iframe
-				width='720'
-				height='405'
 				src='https://www.youtube.com/embed/JigVBj6Pd9I?si=LWZB8sEh0Q-osUc9'
 				title='YouTube video player'
 				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
 				referrerPolicy='strict-origin-when-cross-origin'
 				allowFullScreen
-				className='rounded-box mx-auto shadow-md'
+				className='w-full max-w-[720px] aspect-video rounded-box mx-auto shadow-md'
 			></iframe>
 		</section>
 	);
